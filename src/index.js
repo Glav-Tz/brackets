7df6d3b5a2aa675b9ea3ff191c73ee9549df3efb
Glav-Tz <GlavTz@gmail.com>
2022-02-20 08:57:14 +0300

    finish tasck

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = str;
+  let isHaveNextStep = true;
+
+  const configToString = bracketsConfig.map((element) => {
+    return element.join('')
+  });
+
+  while (isHaveNextStep) {
+    isHaveNextStep = false;
+
+    for (let config of configToString) {
+      const previousResult = result;
+      result = result.replace(config, '');
+
+      if (previousResult !== result) {
+        isHaveNextStep = true;
+        break;
+      }
+    }
+  }
+  return result.length === 0;
 }
